aaf3d0b42972876eafd4dd10b3899ff75ee41d7e
Steven Saus <steven@stevesaus.com>
2022-08-01 20:39:49 -0500

    added busy room file

--- clean_synapse.sh
+++ clean_synapse.sh
@@ -3,6 +3,7 @@
 #https://github.com/matrix-org/synapse/blob/develop/docs/admin_api/purge_history_api.md
 #https://github.com/matrix-org/synapse/tree/develop/docs/admin_api
 
+export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 TEMPDIR=$(mktemp -d)
 
 # Order of precedence
@@ -15,14 +16,14 @@ TEMPDIR=$(mktemp -d)
 API_ID=
 HOMESERVER=
 
-if [ -z API_ID ];then
+if [ -z $API_ID ];then
     if [ -f "${XDG_CONFIG_HOME}/synapse_apikey" ];then
-        API_ID=$(head "${XDG_CONFIG_HOME}/synapse_apikey" -1 | awk -F '=' '{print $2}')
-        HOMESERVER=$(tail "${XDG_CONFIG_HOME}/synapse_apikey" -1 | awk -F '=' '{print $2}')
+        API_ID=$(head -n 1 "${XDG_CONFIG_HOME}/synapse_apikey" | awk -F '=' '{print $2}')
+        HOMESERVER=$(tail -n 1 "${XDG_CONFIG_HOME}/synapse_apikey" | awk -F '=' '{print $2}')
     fi
 fi
 
-if [ -z API_ID ];then
+if [ -z $API_ID ];then
     if [ "$#" -lt 2 ];then
         echo " ./clean_synapse.sh API_ID HOMESERVER_URL"
         echo " The homeserver URL should include https://"
@@ -31,6 +32,11 @@ if [ -z API_ID ];then
         HOMESERVER="$2"
     fi
 fi
+
+echo "${API_ID}"
+echo "${HOMESERVER}"
+read
+
 
 curl --header "Authorization: Bearer ${API_ID}" "${HOMESERVER}/_synapse/admin/v1/rooms?limit=300" > "${TEMPDIR}"/roomlist.json
 
@@ -47,18 +53,21 @@ for room_id in $rooms_to_remove; do
     fi
 done    
 
-# This list is hardcoded in for rooms; there should be a busy rooms list file
-echo -e "\n#################################################################\nRemoving recent history for busy IRC rooms\n"
+if [ -f "${SCRIPT_DIR}"/busy_rooms.cfg ];then
+    echo -e "\n#################################################################\nRemoving recent history for busy IRC rooms\n"
+    rooms_to_clean=$(awk -F '"' '{print $2}' < "${SCRIPT_DIR}"/busy_rooms.cfg)
+    ts=$(( $(date --date="1 days ago" +%s)*1000 ))
+    for room_id in $rooms_to_clean; do 
+    #remove history    
+        echo -e "\nRemoving history for $room_id\n"
 
-ts=$(( $(date --date="1 days ago" +%s)*1000 ))
+        curl --header "Authorization: Bearer ${API_ID}" -X POST -H "Content-Type: application/json" -d "{ \"delete_local_events\": true, \"purge_up_to_ts\": $ts }"  "${HOMESERVER}/_synapse/admin/v1/purge_history/\${room_id}"  
 
-curl --header "Authorization: Bearer ${API_ID}" -X POST -H "Content-Type: application/json" -d "{ \"delete_local_events\": true, \"purge_up_to_ts\": $ts }"  "${HOMESERVER}/_synapse/admin/v1/purge_history/\!ROHxGFSbUrPIKdNrOS:faithcollapsing.com"
-curl --header "Authorization: Bearer ${API_ID}" -X POST -H "Content-Type: application/json" -d "{ \"delete_local_events\": true, \"purge_up_to_ts\": $ts }"  "${HOMESERVER}/_synapse/admin/v1/purge_history/\!FZcWVJryxKhrhFHZrt:libera.chat"
-curl --header "Authorization: Bearer ${API_ID}" -X POST -H "Content-Type: application/json" -d "{ \"delete_local_events\": true, \"purge_up_to_ts\": $ts }"  "${HOMESERVER}/_synapse/admin/v1/purge_history/\!WmSmfjpDqHEiFEJPTL:faithcollapsing.com"
-curl --header "Authorization: Bearer ${API_ID}" -X POST -H "Content-Type: application/json" -d "{ \"delete_local_events\": true, \"purge_up_to_ts\": $ts }"  "${HOMESERVER}/_synapse/admin/v1/purge_history/\!TCEUixVAIbUBUlhRbD:faithcollapsing.com"
+    #the last bit has \${room_id} so the leading ! doesn't escape out
+
+    done
 
 echo -e "\n#################################################################\nRemoving less recent history for all rooms\n"
-
 
 jq '.rooms[] | select(.joined_local_members != 0) | .room_id' < "${TEMPDIR}"/roomlist.json > "${TEMPDIR}"/history_purge.txt 
 
